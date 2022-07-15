4010cd70cd90263c86aa172f302d387880c9c6eb
Anonymous-BCFED <nope@no.pe>
2022-07-14 18:31:22 -0700

    Missing files

---
+++ README-dist.md
@@ -0,0 +1,29 @@
+# AnonymousBCFED_InfoDumper
+
+This is a simple mod for Lilith's Throne that generates JSON data files for use with save editors.
+
+## License
+
+THis mod and plugin are available to you under the Terms of the [MIT Open-Source License Agreement](https://github.com/Anonymous-BCFED/lilithsthrone-mod-infodumper/blob/main/LICENSE).
+
+## Installing
+
+Extract the contents of the archive such that the `res` directory is in the same directory as `LilithsThrone_#.#.#.#.jar`.
+
+You've done it correctly if the structure looks like this:
+
+```
+.
+├── LilithsThrone_#_#_#_#.jar
+└── res
+    └── mods
+        └── anonymousbcfed_infodumper
+            ├── LICENSE
+            ├── plugins
+            │   └── infodumper.jar
+            └── README.md
+```
+
+## Usage
+
+This will automatically dump the files when you load into a game or start a new one.  You'll know it's working by the output in console.
---
+++ src/plugin/infodumper/FetishDumper.java
@@ -0,0 +1,100 @@
+package plugin.infodumper;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.json.JSONException;
+import org.json.JSONWriter;
+
+import com.lilithsthrone.game.character.fetishes.Fetish;
+import com.lilithsthrone.game.character.npc.NPC;
+import com.lilithsthrone.game.character.npc.misc.GenericFemaleNPC;
+import com.lilithsthrone.game.character.npc.misc.GenericMaleNPC;
+
+public class FetishDumper {
+
+	private static NPC malenpc;
+	private static NPC femalenpc;
+
+	public static void dump() {
+		malenpc = new GenericMaleNPC(false);
+		femalenpc = new GenericFemaleNPC(false);
+		dumpFetishes();
+	}
+
+	private static void dumpFetishes() {
+		Path newFilePath = Paths.get("data", "internalData", "fetishes.json");
+		File parentDir = newFilePath.getParent().toFile();
+		if (!parentDir.isDirectory())
+			parentDir.mkdirs();
+		try (FileWriter w = new FileWriter(newFilePath.toFile())) {
+			// I tried using JSONObject and it just ran out of memory. :(
+			JSONWriter json = new JSONWriter(w);
+			// Start global object
+			json.object();
+			// Fart out our header
+			json.key("version").value(1);
+
+			// Begin the perks dict
+			json.key("fetishes");
+			json.object();
+			Fetish.getAllFetishes().forEach(f -> {
+				if (f == null)
+					return;
+				json.key(f.getID());
+				json.object();
+				//json.key("id").value(f.getID());
+				json.key("name"); {
+					json.object();
+					json.key("male").value(f.getName(malenpc));
+					json.key("female").value(f.getName(femalenpc));
+					json.endObject();
+				}
+				json.key("associatedCorruptionLevel").value(f.getAssociatedCorruptionLevel().toString());
+				json.key("attributeModifiers");
+				f.getAttributeModifiers().forEach((k,v) -> {
+					json.key(k.toString());
+					json.value(v);
+				});
+				json.key("cost").value(f.getCost());
+				json.key("description"); {
+					json.object();
+					json.key("male").value(f.getDescription(malenpc));
+					json.key("female").value(f.getDescription(femalenpc));
+					json.endObject();
+				}
+				json.key("experienceGainFromSexAction").value(f.getExperienceGainFromSexAction());
+				json.key("extraEffects");{
+					json.object();
+					json.key("male"); {
+						json.array();
+						f.getExtraEffects(malenpc).forEach(l -> json.value(l));
+						json.endArray();
+					}
+					json.key("female"); {
+						json.array();
+						f.getExtraEffects(femalenpc).forEach(l -> json.value(l));
+						json.endArray();
+					}
+					json.endObject();
+				}
+				json.key("name");{
+					json.object();
+					json.key("male").value(f.getName(malenpc));
+					json.key("female").value(f.getName(femalenpc));
+					json.endObject();
+				}
+				json.endObject();
+			});
+			json.endObject();
+			json.endObject();
+		} catch (JSONException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+}
